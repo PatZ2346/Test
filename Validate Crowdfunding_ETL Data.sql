7900dcb389780c9d4e571a9247a6222dbbacc7f8
PatZ2346 <patrickchou9010@gmail.com>
2024-11-13 19:06:05 +1100

    Complete

---
+++ Validate Crowdfunding_ETL Data.sql
@@ -0,0 +1,7 @@
+SELECT * FROM "Category";
+
+SELECT * FROM "Subcategory";
+
+SELECT * FROM "Contacts";
+
+SELECT * FROM "Campaign";
---
+++ crowdfunding_db_schema.sql.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE "Category" (
+    "category_id" VARCHAR(10) PRIMARY KEY,
+    "category" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" VARCHAR(10) PRIMARY KEY,
+    "subcategory" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" INT PRIMARY KEY,
+    "first_name" VARCHAR(255) NOT NULL,
+    "last_name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" SERIAL PRIMARY KEY,
+    "contact_id" INT NOT NULL,
+    "company_name" VARCHAR(255) NOT NULL,
+    "description" TEXT NOT NULL,
+    "goal" DECIMAL NOT NULL,
+    "pledged" DECIMAL NOT NULL,
+    "outcome" VARCHAR(50) NOT NULL,
+    "backers_count" INT NOT NULL,
+    "country" VARCHAR(2) NOT NULL,
+    "currency" VARCHAR(3) NOT NULL,
+    "launch_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "category_id" VARCHAR(10),
+    "subcategory_id" VARCHAR(10),
+    FOREIGN KEY ("category_id") REFERENCES "Category" ("category_id"),
+    FOREIGN KEY ("subcategory_id") REFERENCES "Subcategory" ("subcategory_id")
+);
